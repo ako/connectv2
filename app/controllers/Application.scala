466c122053d30771973a78e93e1b70caf8cd0485
Andrej Koelewijn <andrej.koelewijn@it-eye.nl>
2012-09-01 07:42:08 +0200

    domain switching

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,7 @@ import play.api.data.Forms._
 import play.api.libs.concurrent._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok("HelloWorld!")
   }
@@ -17,25 +17,26 @@ object Application extends Controller {
     Ok("test1")
   }
 
-  def test2(name:String) = Action {
+  def test2(name: String) = Action {
     Ok("test2" + name)
   }
 
   def login = Action {
-    // prod
-//    val redirectUri = "http://connectv2.herokuapp.com/login/callback"
-//    val jsOrigin = "http://connectv2.herokuapp.com"
+    request =>
+      Logger.info("login: " + request.domain + ", " + request.host)
+      val clientId = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel.apps.googleusercontent.com"
+      val email = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel@developer.gserviceaccount.com"
+      val secret = "0TCVA2LxmErvylnRrcNkR7qH"
+      var redirectUri = "http://connectv2.herokuapp.com/login/callback"
+      var jsOrigin = "http://connectv2.herokuapp.com"
 
-    // dev
-    val clientId = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel.apps.googleusercontent.com"
-    val email = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel@developer.gserviceaccount.com"
-    val secret = "0TCVA2LxmErvylnRrcNkR7qH"
-    val redirectUri = "http://localhost:9000/login/callback"
-    val jsOrigin = "http://localhost:9000/"
-
-    val scope = "https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile"
-    val state = "/profile"
-    Ok(views.html.login(clientId,redirectUri,scope,state))
+      if (request.domain.equals("localhost")) {
+        redirectUri = "http://localhost:9000/login/callback"
+        jsOrigin = "http://localhost:9000/"
+      }
+      val scope = "https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile"
+      val state = "/profile"
+      Ok(views.html.login(clientId, redirectUri, scope, state))
   }
 
   def callback = Action {
