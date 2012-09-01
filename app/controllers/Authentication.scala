6dd0b80a124d2510d51b30cb93833a88cac8fcf6
Andrej Koelewijn <andrej.koelewijn@it-eye.nl>
2012-09-01 22:59:30 +0200

    Fixed redirect

--- app/controllers/Authentication.scala
+++ app/controllers/Authentication.scala
@@ -21,7 +21,7 @@ import play.api.libs.ws.WS
 object Authentication extends Controller {
 
   val clientId = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel.apps.googleusercontent.com"
-//  val email = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel@developer.gserviceaccount.com"
+  //  val email = "672117013692-pnps2esp4nh7ejcjbb79mta6cdtb7sel@developer.gserviceaccount.com"
   val secret = "0TCVA2LxmErvylnRrcNkR7qH"
   var redirectUri = "http://connectv2.herokuapp.com/login/callback"
   var jsOrigin = "http://connectv2.herokuapp.com"
@@ -51,7 +51,7 @@ object Authentication extends Controller {
 
       Logger.info("code: " + code + ", state: " + state)
       var accessToken: String = null
-      var username:String = null
+      var username: String = null
       if (state.equals("/profile")) {
         if (request.domain.equals("localhost")) {
           redirectUri = "http://localhost:9000/login/callback"
@@ -63,7 +63,7 @@ object Authentication extends Controller {
       }
       Logger.info("done: " + accessToken)
       //Ok(views.html.auth("hi there!" + accessToken))
-      Redirect("http://localhost:9000/assets/app2/connect.html").withSession("access_token" -> accessToken)
+      Redirect("http://" + request.host + "/assets/app2/connect.html").withSession("access_token" -> accessToken)
   }
 
   def getAccessToken(code: String): String = {
@@ -79,24 +79,24 @@ object Authentication extends Controller {
     val tokenBody = promise.value.get.body
     val json: JsValue = Json.parse(tokenBody)
     val access_token = json.\("access_token").as[String]
-    val id_token =( json \ "id_token").as[String]
+    val id_token = (json \ "id_token").as[String]
     Logger.info("tokens: " + access_token + ", " + id_token)
     return access_token.toString()
   }
 
-  def getUsername(accessToken:String):String = {
+  def getUsername(accessToken: String): String = {
     Logger.info("getUsername: " + accessToken)
 
-    val promise: Promise[ws.Response] = WS.url("https://www.googleapis.com/oauth2/v1/userinfo?access_token="+accessToken).get()
+    val promise: Promise[ws.Response] = WS.url("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + accessToken).get()
     Logger.info("get")
     val body = promise.value.get.body
     Logger.info("body: " + body)
-//    Logger.info(body.toString())
-//    val json: JsValue = Json.parse(tokenBody)
-//    val access_token = json.\("access_token")
-//    val id_token = json \ "id_token"
-//    Logger.info("tokens: " + access_token + ", " + id_token)
-//    return access_token.toString()
+    //    Logger.info(body.toString())
+    //    val json: JsValue = Json.parse(tokenBody)
+    //    val access_token = json.\("access_token")
+    //    val id_token = json \ "id_token"
+    //    Logger.info("tokens: " + access_token + ", " + id_token)
+    //    return access_token.toString()
     (Json.parse(body) \ "name").as[String]
   }
 
